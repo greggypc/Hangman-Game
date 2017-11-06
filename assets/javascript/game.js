b09817c1f3042c490b5e182e9e364709c75fc5e7
Greg Church <greggypc@users.noreply.github.com>
2017-11-06 00:02:37 -0600

    progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,8 @@
 
 // Hangman Game
 
-
-	
-	
 	var guessCount = 0;
 	var gameWins = 6;
-	
 	
 
 	function gameStart() {
@@ -15,47 +11,56 @@
 		var gameLetters = thisGameWord.split("");
 		
 		var blanks = gameLetters.map(i=>' _ ');
-		console.log(blanks);
+		// console.log(blanks);
+		// console.log(blanks.join(" "));
 		document.getElementById("demo").innerHTML = blanks.join(" ");
-	}
+
+	
 
 	 document.onkeyup = function(event) {
-
-      // Determines which key was pressed.
+     
       var userGuess = event.key;
-      for (i = 0; i < blanks; i++) {
-      	if (userGuess == gameLetters[i]) {
-      		//replace blank with userGuess
-      	}
-      	else {
-      		//push letter into (blank) array and print array to screen
-      	}
+      var startValue = 0;
+     		
+	      while (gameLetters.indexOf(userGuess, startValue) !== -1) {
+	      		
+	      		// if in word .push to array gameLetters and replace blank(s) with letter(s)
+	      		blanks[gameLetters.indexOf(userGuess, startValue)].push(userGuess);
+	      		startValue = gameLetters.indexOf(userGuess, startValue) + 1; 
+			}
+	      		
+	      		// else - .push to empty junkArray AND add 1 to guessCount
+	      if (gameLetters.indexOf(userGuess, startValue) === -1) {
+	      		var junkArray = [];
+	      		junkArray.push(userGuess);
+	      		guessCount++;
+	      	}	
+
+	      		// if userGuess is in junkArray - alert("Already used! Pick another letter");
+	      if (junkArray.indexOf(userGuess, 0) !== -1) {
+	      		alert("Already used! Select another letter.");
+	      	}	
+				
       };
+	}
+      
+
+
+      function trackWins() {
+      	document.getElementById('wins').innerHTML = gameWins;
+      }
       
 
        // document.querySelector("#game").innerHTML = "You chose: " + userGuess + ". The computer chose: " + computerGuess;
-    
+    	//var res = str.toUpperCase();
+    	// document.querySelector("#gameLetters").innerHTML = gameLetters;
 
-
-	
-
-	
-// document.querySelector("#gameLetters").innerHTML = gameLetters;
-	// 
 // var gameLetters = document.getElementById('gameLetters');
 
 	
 	// While (guessCount <= 10) {  //while game still going
 
-// console.log(gameWords);
-// console.log(gameLetters);
-	// 	while (gameWord.indexOf(guess, startValue) !== -1) {
-	// 	startValue = word.indexOf(guess,startValue) +1;
 
-	//  document.onkeyup = function(event) {
-
- //      // Determines which key was pressed.
- //      var userGuess = event.key;
 
       
  
