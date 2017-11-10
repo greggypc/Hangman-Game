20eab9dff98e52e36c2fb50ce8635a1ce84120ac
Greg Church <greggypc@users.noreply.github.com>
2017-11-09 21:54:21 -0600

    code works - need css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,7 +9,7 @@
 		document.getElementById("guessCount").innerHTML = guessCount;
 		document.getElementById("gameWins").innerHTML = gameWins;
 		return;
-	}
+	};
 	
 
 	function gameStart() {
@@ -20,7 +20,7 @@
 		document.getElementById("guessCount").innerHTML = guessCount;
 		document.getElementById("junkArray").innerHTML = junkArray.join(" ");
 		//var gameWords = ["AA", "BBBBBB"];
-		var gameWords = [ "ARCHIPELAGO"];
+		var gameWords = [ "ARCHIPELAGOEP"];
 		//var gameWords = ["CANTALOUPE", "ARCHIPELAGO", "BASKETBALL", "NOTEBOOK"];
 		var thisGameWord = gameWords[Math.floor(Math.random() * gameWords.length)];
 		var gameLetters = thisGameWord.split("");
@@ -33,32 +33,50 @@
 	  document.onkeyup = function(event) {
       var userGuess = event.key.toUpperCase();
       var startValue = 0;
-      console.log(userGuess);	
+     
 	      
 	      while (gameLetters.indexOf(userGuess, startValue) !== -1) {
 	      		
-	      		// if in word .push to array gameLetters and replace blank(s) with letter(s)
+	      		// if in word print to array gameLetters and replace blank(s) with letter(s)
 	      		blanks[gameLetters.indexOf(userGuess, startValue)] = userGuess;
-	      		// if (isCorrect = true) {
-	      			
-	      		// }	      		
+	      		//blanks[gameLetters.indexOf(userGuess, startValue)].push(userGuess);   		
 	      		document.querySelector("#gameBlanks").innerHTML = blanks.join(" ");
 	      		startValue = gameLetters.indexOf(userGuess, startValue) + 1; 
-	      		guessCount--;
+	      		//guessCount--;
 	      	}
 
-	      	//array.push(b);
-			//return array;	
+	      	// function won() {
+	      	//var gameWins = 0;
+	      	var startValue = 0;
+	      	while (blanks.indexOf(' _ ', startValue) === -1) {
+	      		alert("You won!");
+	      		gameWins++;
+	      		document.getElementById("gameWins").innerHTML = gameWins;
+	      		return;
+	      	}
 
+	      	if (guessCount < 2 ) {
+	      		alert("You lose! Press 'START' to play again.")
+	      	}
+	      // };
+
+	      	
+	      
+
+	      	// function usedAlready() {
 	      	// if userGuess is in junkArray (0r  || is in gameletters?) must exct. before add to junkArray
 	      	if (junkArray.indexOf(userGuess, 0) !== -1 ) { 
 	      		alert("Already used! Select another letter.");
-	      		userGuess = "";
+	      		userGuess = ""; // keep repeated guess from being added to junkArray
 	      		guessCount++; //keeps guessCount from deducting on repeater guess
-	      		// keep repeated guess from being added to junkArray
+	      		
 	      	}
+	     // }; // end usedAlready
 
-      		// else - .push to empty junkArray AND add 1 to guessCount
+
+	     // function wrongLetter() {
+      		// if guess not in word print to empty junkArray AND deduct 1 from remaining guessCount
+      		var startValue = 0;
 	        if (gameLetters.indexOf(userGuess, startValue) === -1) {
 	      		
 	      		junkArray.push(userGuess);
@@ -67,63 +85,14 @@
 	      		guessCount--;
 	      		document.getElementById("guessCount").innerHTML = guessCount;
 	      	}	
+	     // };  // end wrongLetter
 
 // ^^^^^ repeated letter should not appear in junkArray or cause guessCount to deduct	!!!!!!^^^^^      	
 
-		};  //end keyup function
+		}; // end keyup function
 		
 }  //end main function
 		
-
-
-// if user wins
-		
-		// function trackWins() {
-	 //      if(array of blanks is filled && guessCount >= 1 --- you win!){
-	 //      	gameWins++;
-	 //      	document.getElementById("winCount").innerHTML = gameWins;
-	 //      }	
-				
-     // function trackWins() {
-      // 	document.getElementById('wins').innerHTML = gameWins;
-      // }
-
-
-       // for (var i = 0; i < myFarm.length; i++) {
- //        var animal = myFarm[i];
- //        var anName = myFarm[i];
-
- //          if( animal.charAt() == "c" || animal.charAt() == "o"){
- //        alert(anName + " starts with a c or o!");
- //        }
- //        else {
- //          alert(anName + " doesn't");
- //        } 
- //      }
-
-
-      
-
-       // document.querySelector("#game").innerHTML = "You chose: " + userGuess + ". The computer chose: " + computerGuess;
-    	//var res = str.toUpperCase();
-    	// document.querySelector("#gameLetters").innerHTML = gameLetters;
-
-// var gameLetters = document.getElementById('gameLetters');
-
-	
-	// While (guessCount <= 10) {  //while game still going
-
-
-
-      
- 
-
-
-	
-
-
-
-
 
 
 
