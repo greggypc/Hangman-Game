e9b95ae5616f0dca91f3818f2c12ed43260c2977
Greg Church <greggypc@users.noreply.github.com>
2017-11-11 05:51:21 -0600

    adding images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,6 +10,7 @@
 		document.getElementById("guessCount").innerHTML = "Remaining Guesses: " + guessCount;
 		document.getElementById("gameWins").innerHTML = "Wins: " + gameWins;
 		document.getElementById("junkArray").innerHTML = "Letters Used: " + junkArray.join(" ");
+		var manImg = $('#imgDiv').html('<img id="manImg" src="assets/images/hangman.png" />')
 		return;
 	};
 	
@@ -18,7 +19,7 @@
 		
 		var guessCount = 10;
 		var junkArray = [];
-		
+		var manImg = $('#imgDiv').html('<img id="manImg" src="assets/images/hangman-01.png" />');
 		document.getElementById("guessCount").innerHTML = "Remaining Guesses: " + guessCount;
 		document.getElementById("junkArray").innerHTML = "Letters Used: " + junkArray.join(" ");
 		var gameWords = ["PTERODACTYL", "JAVASCRIPT", "CANTALOUPE",
@@ -42,20 +43,37 @@
 	      		// if in word print to array gameLetters and replace blank(s) with letter(s)
 	      		blanks[gameLetters.indexOf(userGuess, startValue)] = userGuess;
 	      		document.querySelector("#gameBlanks").innerHTML = blanks.join(" ");
+	      		 // Sound effect
+			      var audioElement = document.createElement("audio");
+			      audioElement.setAttribute("src", "assets/audio/Good-idea-bell.mp3");
+			      audioElement.play();
 	      		startValue = gameLetters.indexOf(userGuess, startValue) + 1; 
 	      	}
 
-      	
+
+
 	      	var startValue = 0;
 	      	while (blanks.indexOf(' _ ', startValue) === -1) {
-	      		gameWins++;
+	      		//You win
+	      		// Sound effect
+			      var audioElement = document.createElement("audio");
+			      audioElement.setAttribute("src", "assets/audio/Magic-zing-sound-effect.mp3");
+			      audioElement.play();
+			      gameWins++;
 	      		document.getElementById("gameWins").innerHTML = "Wins: " + gameWins;
-	      		alert("You win! Press 'START' to play again.");
+	      		gameStart();
 	      		return;
 	      	}
 
+	      
 	      	if (guessCount < 2 ) {
-	      		alert("You lose! Press 'START' to play again.")
+	      		//You lose
+	      		// Sound effect
+			      var audioElement = document.createElement("audio");
+			      audioElement.setAttribute("src", "assets/audio/Wha-wha-wha-sound-effect.mp3");
+			      audioElement.play();
+			      gameStart();
+			      return;
 	      	}
 	    
 	      	
@@ -64,10 +82,22 @@
 	      		alert("Already used! Select another letter.");
 	      		userGuess = ""; // keep repeated guess from being added to junkArray
 	      		guessCount++; //keeps guessCount from deducting on repeater guess
-	      		
+	      		return;
 	      	}
 	    
-
+	      	 var image = [
+		'assets/images/hangman.png',
+		'assets/images/hangman10.png',
+		'assets/images/hangman09.png',
+		'assets/images/hangman08.png',
+		'assets/images/hangman07.png',
+		'assets/images/hangman06.png',
+		'assets/images/hangman05.png',
+		'assets/images/hangman04.png',
+		'assets/images/hangman03.png',
+		'assets/images/hangman02.png',
+		'assets/images/hangman01.png'
+		] 
 
 	     
       		// if guess not in word print to empty junkArray AND deduct 1 from remaining guessCount
@@ -76,9 +106,13 @@
 	      		
 	      		junkArray.push(userGuess);
 	      		document.getElementById("junkArray").innerHTML = "Letters Used: " + junkArray.join(" ");
-	     		
+	     		 // Sound effect
+			      var audioElement = document.createElement("audio");
+			      audioElement.setAttribute("src", "assets/audio/Failure-sound-effect.mp3");
+			      audioElement.play();
 
 	      		guessCount--;
+	      		var manImg = $('#imgDiv').html('image[guessCount]');
 	      		document.getElementById("guessCount").innerHTML = "Remaining Guesses: " + guessCount;
 	      	}	
 	    	
@@ -86,24 +120,18 @@
 		}; // end keyup function
 
 }  //end main function
-		
-//  swap hangman image for next image
-		// 	     var image = [
-		// 'assets/images/hangman.png',
-		// 'assets/images/hangman10.png',
-		// 'assets/images/hangman09.png',
-		// 'assets/images/hangman08.png',
-		// 'assets/images/hangman07.png',
-		// 'assets/images/hangman06.png',
-		// 'assets/images/hangman05.png',
-		// 'assets/images/hangman04.png',
-		// 'assets/images/hangman03.png',
-		// 'assets/images/hangman02.png',
-		// 'assets/images/hangman01.png'
-		// ] 
 
-  //       //var bodyPic = $("<div>").attr("data-letter", letters[i]).text(letters[i]);
-  //      // $(fridgeMagnet).appendTo("#display");
+				// var losses = 0;
+				// losses++;
+	   //    		var percent = (gameWins / losses) * 100;
+	   //    		document.getElementById("percent").innerHTML = percent + "%";		
+
+
+ //swap hangman image for next image
+			    
+
+        //var bodyPic = $("<div>").attr("data-letter", letters[i]).text(letters[i]);
+       // $(fridgeMagnet).appendTo("#display");
   //     	  $(#addBodyPart).html(function(e){
 	 //     var addLimb = e.addLimb;
   // 		 $("#pic").attr('src', image[addLimb]);
